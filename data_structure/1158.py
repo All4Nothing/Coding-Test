aa4817c829609af92026fa83a4ac8b5e4f1e7fe1
YongJoo Kim <sd081104@gmail.com>
2023-08-14 06:44:11 +0000

    Done by 23.08.14

--- data_structure/1158.py
+++ data_structure/1158.py
@@ -4,8 +4,9 @@ from collections import deque
 
 n, k = map(int, input().split())
 n_list = deque([i for i in range(1, n+1)])
-print('<', end='')
-while len(n_list) > 1:
+l=['<']
+while len(n_list):
     n_list.rotate(-k+1)
-    print(n_list.popleft(),end=', ')
-print(*n_list, '>', sep='')
+    l.append(n_list.popleft())
+    l.append(', ')
+print(*l[:-1],'>', sep='')
