abb4b6f2b1be8e57befeffb1b105dd8973bf8da9
YongJoo Kim <sd081104@gmail.com>
2023-09-19 13:31:47 +0000

    Done by 23.09.19

---
+++ data_structure2/4358.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+d = {}
+
+while True:
+    tree = input().rstrip()
+    if tree:
+        if tree in d:
+            d[tree] += 1
+        else:
+            d[tree] = 1
+    else:
+        break
+
+s_dict = dict(sorted(d.items()))
+n = sum(s_dict.values())
+for item in s_dict:
+    print(item, f'{s_dict[item]/n*100:.4f}')
