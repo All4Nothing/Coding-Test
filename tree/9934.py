d3d62110527b4640787c5fd0d8a6c1c2340c61e2
YongJoo Kim <sd081104@gmail.com>
2023-10-10 12:32:07 +0000

    Done by 23.10.10

--- tree/9934.py
+++ tree/9934.py
@@ -4,9 +4,9 @@ arr = list(map(int, input().split()))
 tree = [None for i in range(2**k-1)]
 
 def make_tree(arr, idx):
-    if idx >= 2**k - 1:
-        return True
-    mid = int(len(arr)/2)
+    if idx >= 2**k - 1: #
+        return 
+    mid = len(arr) // 2 
     tree[idx] = arr[mid]
     make_tree(arr[:mid], idx*2 + 1)
     make_tree(arr[mid+1:], idx*2 + 2)
@@ -14,6 +14,6 @@ def make_tree(arr, idx):
 make_tree(arr, 0)
 
 l = 0
-while l < 2*k:
+while l < 2**k:
     print(*tree[l:l*2+1])
     l = l*2 + 1
